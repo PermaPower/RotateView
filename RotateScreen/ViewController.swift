fd08f4ccb15ad70106647145eb252bd86357723a
David <David@MacBook-Pro.local>
2017-02-10 22:33:15 +1100

    centertext but need to fix location to centre on rotation

--- RotateScreen/ViewController.swift
+++ RotateScreen/ViewController.swift
@@ -10,23 +10,29 @@ import UIKit
 
 class ItemViewController: UIViewController {
     
+    // Upon rotation of device, invalidateLayout
     override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
         collectionView.collectionViewLayout.invalidateLayout()
         }
 
+    // Setup cellID for collectionView
     let cellId = "cellID"
+    
+    // Month names
+    let months: [String] = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
     
     let collectionView: UICollectionView = {
     
         let layout = UICollectionViewFlowLayout()
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        
+        cv.allowsMultipleSelection = true
         cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.isScrollEnabled = false
         
         return cv
         
     }()
-    
+
     override func viewDidLoad() {
         
         super.viewDidLoad()
@@ -43,23 +49,40 @@ class ItemViewController: UIViewController {
         
         view.addSubview(collectionView)
         
-         view.addConstraintsWithFormat(format: "H:|[v0]|", views: collectionView)
-         view.addConstraintsWithFormat(format: "V:|[v0]|", views: collectionView)
+        view.addConstraintsWithFormat(format: "H:|[v0]|", views: collectionView)
+        view.addConstraintsWithFormat(format: "V:|[v0]|", views: collectionView)
+
+        
     }
 }
 
 
 extension ItemViewController: UICollectionViewDataSource {
     
+    // Return the number of cells in collecitonView
     @available(iOS 6.0, *)
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 12
     }
 
+    // Modify the cell contents here
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-        cell.backgroundColor = .blue
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath as IndexPath) as UICollectionViewCell
+        
+        cell.backgroundColor  = .blue
+        
+        let textLabel = UILabel(frame: CGRect(x:0, y:0, width:cell.frame.size.width, height:cell.frame.size.height))
+        textLabel.textColor = UIColor.white
+        textLabel.adjustsFontSizeToFitWidth = true
+        textLabel.textAlignment = .center
+        textLabel.layer.cornerRadius = 10
+        textLabel.layer.borderWidth = 1
+        textLabel.layer.borderColor = UIColor.white.cgColor
+        textLabel.layer.masksToBounds = true
+        textLabel.text = months[indexPath.row]
+        cell.contentView.addSubview(textLabel)
+        
         return cell
     }
     
@@ -73,6 +96,7 @@ extension ItemViewController: UICollectionViewDelegateFlowLayout {
         let margings = 2 * (columns - 1)
         let width = (collectionView.frame.size.width - CGFloat(margings)) / CGFloat(columns)
         let height = width // square cells
+        
         return CGSize(width: width, height: height)
     }
     
@@ -84,6 +108,7 @@ extension ItemViewController: UICollectionViewDelegateFlowLayout {
         return 0
     }
     
+    // Insert UIEdge to push collectionview down to center of view
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         
         let columns = 6
@@ -98,4 +123,3 @@ extension ItemViewController: UICollectionViewDelegateFlowLayout {
     }
     
 }
-    
