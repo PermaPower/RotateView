a8a1d3776429e8be0e905fc4731955d4fd23b64d
David <David@MacBook-Pro.local>
2017-02-10 20:36:39 +1100

    Rotating center UIView

--- RotateScreen/ViewController.swift
+++ RotateScreen/ViewController.swift
@@ -8,18 +8,93 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ItemViewController: UIViewController {
+    
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        collectionView.collectionViewLayout.invalidateLayout()
+        }
 
+    let cellId = "cellID"
+    
+    let collectionView: UICollectionView = {
+    
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        
+        return cv
+        
+    }()
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupController()
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupController() {
+        
+        view.addSubview(collectionView)
+        
+         view.addConstraintsWithFormat(format: "H:|[v0]|", views: collectionView)
+         view.addConstraintsWithFormat(format: "V:|[v0]|", views: collectionView)
     }
-
-
 }
 
+
+extension ItemViewController: UICollectionViewDataSource {
+    
+    @available(iOS 6.0, *)
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+}
+
+extension ItemViewController: UICollectionViewDelegateFlowLayout {
+    
+    // Divide the collectionview by 6 x 2
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let columns = 6
+        let margings = 2 * (columns - 1)
+        let width = (collectionView.frame.size.width - CGFloat(margings)) / CGFloat(columns)
+        let height = width // square cells
+        return CGSize(width: width, height: height)
+    }
+    
+    // Reduce minimum line spacing between collectionview cells to zero
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        let columns = 6
+        let totalCellWidth = Float(collectionView.bounds.width) / Float(columns)
+        let totalCellHeight = totalCellWidth * 2
+
+        let topInset = (self.view.frame.height - CGFloat(totalCellHeight)) / 2
+        let bottInset = (self.view.frame.height - CGFloat(totalCellHeight)) / 2
+        
+        return UIEdgeInsetsMake(topInset, 0, bottInset, 0)
+    }
+    
+}
+    
